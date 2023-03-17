b44baa96886a4495c440059c913e72e7181892b8
MobuchiElly <ugwueleazer@gmail.com>
2023-03-17 22:18:28 +0100

    My First Javascript Project

---
+++ Desktop/GITHUB FOLDER/JavaScript Projects/Project 0.js
@@ -0,0 +1,13 @@
+function Origin()
+{
+    let firstDisplay, secondDisplay;
+    firstDisplay = "Thanks for choosing us."; 
+    secondDisplay = "We value your patronage.";
+    document.getElementById("demo").innerHTML = firstDisplay
+    + "<br> " + secondDisplay;
+}
+
+function myFunction()
+{
+    document.getElementById("demo").innerHTML = "Hello User";
+}
